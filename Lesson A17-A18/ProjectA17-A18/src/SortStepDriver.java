fda1ccfa06daa1a4db65507dc2af439727ff803b
Richard Huang <richardhuang.huang@gmail.com>
2017-03-11 16:40:21 -0800

    fixed formatting

--- Lesson A17-A18/ProjectA17-A18/src/SortStepDriver.java
+++ Lesson A17-A18/ProjectA17-A18/src/SortStepDriver.java
@@ -5,7 +5,7 @@ public class SortStepDriver {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SortStep ss = new SortStep();
+		SortStep ss=new SortStep();
 		ss.sortMenu();
 	}
 
